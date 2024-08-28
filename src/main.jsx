72a501e8b76275805b32915715c3416bd11098fc
DiveshR <divesh20july@gmail.com>
2024-08-28 21:31:29 +0530

    User Auth react routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route  } from 'react-router-dom'
 import App from './App.jsx'
-// import './index.css'
 import './assets/main.css'
+import Home from './views/Home.jsx'
+import Register from './views/auth/Register.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <App /> }>
+          <Route index element={ <Home /> } />
+          <Route path='register' element={ <Register /> } />
+        </Route>  
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
